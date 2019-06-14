b84a5d50549c0a56dea4a37b68c14e24d28f588d
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-06-13 21:29:24 -0600

    Implement first 'Hello World' test on localhost
    This is a bare-bones test of responding with an HTML string.

--- server.js
+++ server.js
@@ -5,4 +5,26 @@ const server = http.createServer();
 
 server.on('request', (request, response) => {
   const requestUrl = url.parse(request.url, true);
+
+  switch(request.method) {
+  case 'GET':
+    if(requestUrl.pathname.startsWith('/')) {
+      return response.end('<p>HTML sample test</p>');
+    } else {
+      return response.writeHead('404').end('Page Not Found');
+    }
+  }
 });
+
+const port = 3000;
+
+server.on('error', e => {
+  console.error(`Server error: ${e}\nShutting down.`);
+  process.exit(1);
+});
+
+server.on('listening', e => {
+  console.log(`Server started, listening on port ${port}`);
+});
+
+server.listen(port);
