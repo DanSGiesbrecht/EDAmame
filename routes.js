a03b9a8a3f454305501b2ca7937fab1f5e5e64ea
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-07-17 21:16:15 -0600

    Rename router.js to routes.js

--- routes.js
+++ routes.js
@@ -1,17 +1,22 @@
-// router.js
+// routes.js
 
 const url = require('url');
+
+// npm packages
 const ejs = require('ejs');
 
 module.exports = {
-  home: (request, response) => {
+  static: (request, response) => {
+
+  },
+  home: async (request, response) => {
     const requestUrl = url.parse(request.url, true);
 
     switch(request.method) {
     case 'GET':
       if(requestUrl.pathname.startsWith('/')) {
         ejs.renderFile('./views/home.ejs', (error, content) => {
-    return response.end(content);
+          return response.end(content);
         });
         //return response.end('<p>HTML sample test</p>');
       } else {
