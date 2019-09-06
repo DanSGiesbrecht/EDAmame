f2bc7fa5d19f5e9158fd7aeb53949b60d4817332
Daniel Giesbrecht <daniel.soley.giesbrecht@gmail.com>
2019-09-06 00:09:32 -0600

    Improve Oauth route, add helper.js
    - Moved OAuth code to digikey.js
    - Removed promise return (doesn't need it yet)
    - Extracted code from query data
    - Moved helper functions to helper.js

--- src/digikey.js
+++ src/digikey.js
@@ -2,6 +2,23 @@
 * digikey.js
 *************************************************************/
 
-const request = require('request');
+//const request = require('request');
 const config = require('./config.js');
-// Test partdetails
+const helper = require('./helper.js');
+
+module.exports = {
+
+  getCode: (request) => {
+    const oauthRedirect = helper.queryData(request.url);
+    if('error' in oauthRedirect) {
+      console.log(oauthRedirect.error);
+      return oauthRedirect.error;
+    } else if('code' in oauthRedirect) {
+      console.log('code: ' + oauthRedirect.code);
+      return oauthRedirect.code;
+    } else {
+      console.log('Did not receive code or error');
+      return 'no_code_or_error';
+    }
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,18 @@
 * routes.js
 *************************************************************/
 
-const url = require('url');
 const fs = require('fs');
 const ejs = require('ejs');
 const nodePath = require('path');
+const digikey = require('./digikey.js');
+const helper = require('./helper.js');
 
 module.exports = {
 
   static: (request, response) => {
     return new Promise((resolve, reject) => {
       let safePath = nodePath.normalize(decodeURI(request.url));
-      if(onPath('/static', safePath) && request.method === 'GET') {
+      if(helper.onPath('/static', safePath) && request.method === 'GET') {
         const mimetypes = { 'css': 'text/css' };
         const filePath = '.' + request.url;
         const ext = filePath.split('.').pop();
@@ -34,23 +35,20 @@ module.exports = {
   },
 
   oauth: (request, response) => {
-    return new Promise((resolve, reject) => {
-      if(onPath('/oauth/redirect', request.url) && request.method === 'GET') {
-	console.log('oauth page');
-	console.log(queryData(request.url));
-	response.end();
-	resolve(true);
-      } else {
-	resolve(false);
-      }
-    });
+    // Note: there is no promise returned yet from getCode! It doesn't yet need it.
+    if(helper.onPath('/oauth/redirect', request.url) && request.method === 'GET') {
+      console.log('oauth page');
+      console.log('getCode: ' + digikey.getCode(request));
+      response.end();
+      return true;
+    } else { return false; }
   },
 
   home: (request, response) => {
     return new Promise((resolve, reject) => {
       switch(request.method) {
       case 'GET':
-        if(onPath('/', request.url)) {
+        if(helper.onPath('/', request.url)) {
           ejs.renderFile('./views/home.ejs', (error, content) => {
 	    response.end(content);
             resolve(true);
@@ -62,18 +60,3 @@ module.exports = {
     });
   }
 };
-
-/*************************************************************
-* helper functions
-*************************************************************/
-
-// onPath
-function onPath(path, urlObj) {
-  const reqUrl = url.parse(urlObj, true);
-  return reqUrl.pathname.startsWith(path);
-}
-
-// queryData
-function queryData(urlObj) {
-  return url.parse(urlObj, true).query;
-}
